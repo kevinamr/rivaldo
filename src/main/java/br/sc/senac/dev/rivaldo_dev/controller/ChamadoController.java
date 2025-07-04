53e9cf0288481ab657d71ffa4c20df593f57abe0
kevinamr <kevinamraulino@gmail.com>
2025-07-04 17:08:36 -0300

    hot fix pra conseguir executar o teste

--- src/main/java/br/sc/senac/dev/rivaldo_dev/controller/ChamadoController.java
+++ src/main/java/br/sc/senac/dev/rivaldo_dev/controller/ChamadoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,8 +36,8 @@ return chamadoService.publicar(novoChamado);
 
 @Operation(summary = "Pesquisar todos os Chamados", 
    description = "Busca todos os chamados disponiveis")
-@GetMapping
-private List<Chamado> procurarChamadosEmAndamento(@RequestParam("email") String email){
+@GetMapping("/{email}")
+private List<Chamado> procurarChamadosEmAndamento(@PathVariable String email){
 List<Chamado> tudo = chamadoService.procurarChamadosEmAndamento(email);
 return tudo;
 }
